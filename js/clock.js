3fb47f06a7a1c0b319d5189f07496bd52cbffa03
Phil Ko <35305115+KeonwooKo@users.noreply.github.com>
2022-07-02 01:20:30 +0900

    chat screen

---
+++ js/clock.js
@@ -0,0 +1,44 @@
+const clock = document.querySelector("div.chat__timestamp");
+const time = document.querySelector("span.message__time");
+
+function getClock() {
+  var day_array = new Array(
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  );
+  var month_array = new Array(
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  );
+
+  let today = new Date();
+
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let month_label = month_array[month];
+  let date = today.getDate();
+  let day = today.getDay();
+  let day_label = day_array[day];
+  let hours = ("0" + today.getHours()).slice(-2);
+  let minutes = ("0" + today.getMinutes()).slice(-2);
+
+  clock.innerText = `${day_label} ,${month_label} ${date}, ${year}`;
+  time.innerText = `${hours}:${minutes}`;
+}
+
+getClock();
